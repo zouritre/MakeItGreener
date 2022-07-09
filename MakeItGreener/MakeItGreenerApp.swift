2203e8fc27e873823e8aded4b6160a9726902613
zouritre <timouf97400@live.fr>
2022-07-09 18:03:10 +0200

    Initial Commit

---
+++ MakeItGreener/MakeItGreenerApp.swift
@@ -0,0 +1,20 @@
+//
+//  MakeItGreenerApp.swift
+//  MakeItGreener
+//
+//  Created by Bertrand Dalleau on 09/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MakeItGreenerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
